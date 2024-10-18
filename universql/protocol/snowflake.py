5835fac9ddbf9c5eefae4a7768da7c055d1ab1cb
Burak Kabakcı <emrekabakci@gmail.com>
2024-10-18 04:20:41 +0100

    Disable sentry debugging

--- universql/protocol/snowflake.py
+++ universql/protocol/snowflake.py
@@ -38,14 +38,11 @@ sentry_sdk.init(
     # Set traces_sample_rate to 1.0 to capture 100%
     # of transactions for tracing.
     traces_sample_rate=1.0,
-    debug=True,
+    debug=False,
     # Set profiles_sample_rate to 1.0 to profile 100%
     # of sampled transactions.
     # We recommend adjusting this value in production.
     profiles_sample_rate=1.0,
-    integrations=[
-        AsyncioIntegration()
-    ]
 )
 
 sessions = {}
